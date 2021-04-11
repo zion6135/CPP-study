1c7ae456739229a458a1f1e55fffb811d831c44d
zion6135 <1159124378@qq.com>
2021-04-11 21:49:09 +0800

    子线程传入参数

--- 1.multithreading/main.cpp
+++ 1.multithreading/main.cpp
@@ -1,14 +1,18 @@
+//g++ main.cpp -l pthread
 #include <iostream>
 #include <thread>
 
 using namespace std;
 
-void func() {
-    cout << "hello, this is my thread == " << this_thread::get_id() <<endl;
+void func(string& s) {
+    cout << s << "  hello, this is my thread == " << this_thread::get_id() <<endl;
 }
 
 int main() {
-    thread th = thread(func);
+    //thread th = thread(func);
+    
+    string s = "test";
+    thread th = thread(func, std::ref(s));//传入参数s   
     th.join();
     cout << "this is main == " << this_thread::get_id()<<endl;
 
