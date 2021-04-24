bbfe815882c791f2db6d88247af90719d4d00af4
liubw0112 <liubw0112@thundersoft.com>
2021-04-24 14:03:42 +0800

    添加消息队列

--- 3.socket-udp/SocketUDP.h
+++ 3.socket-udp/SocketUDP.h
@@ -11,22 +11,34 @@
 #include<arpa/inet.h>
 #include <unistd.h>
 #include <thread>
+#include <pthread.h>
 
-
+#include <deque>
+#include <map>
+#include <deque>
+#include <mutex>
+#include <semaphore.h>
+#include <vector>
 using namespace std;
 
-#define MAX_MSG_SIZE 1024 * 1024 * 2  // 2M
+#define MAX_MSG_SIZE 1024  // 2M
 #define SEVER_PORT 8080
 #define CLIENT_PORT 8080
-#define SEVER_IP "172.20.10.5"
-#define CLIENT_IP "172.20.10.5"
+#define SEVER_IP "172.17.0.1"
+#define CLIENT_IP "127.0.0.1"
+//172.16.0.89
+//172.17.0.1
+typedef struct {
+    int a;
+    char b[10];
+}data_t;
 
 class SocketUDP {
 /*Server端*/
 public:
     int init();
     int main_process();
-    int Reveive_Process(const char* data, const int len);
+    int Reveive_Process( data_t* data, const int len);
 
 private:
     int Server_Init();
@@ -37,7 +49,14 @@ public:
     int sendUDP(const string msg);
 
 
+/*message queue*/
+public:
+    void startProcMsg();
 
+private:
+    vector<data_t*> data_m;  //消息队列
+    mutex m_mutex;  //锁
+    sem_t m_sem;  //信号量
 };
 
 #endif
